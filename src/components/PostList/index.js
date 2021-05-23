88389ed6dba8e84ec98b25c61fda9f06779b8e78
Thales Domingues <thdomingues.dev@gmail.com>
2021-05-22 23:32:31 -0400

    feat: EditButton added to post resource

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
 
 const PostList = (props) => (
   <List {...props}>
-    <Datagrid rowClick="edit">
+    <Datagrid>
+      <TextField source="id" />
+
       <ReferenceField source="userId" reference="users">
         <TextField source="name" />
       </ReferenceField>
-      <TextField source="id" />
+
       <TextField source="title" />
-      <TextField source="body" />
+
+      <EditButton />
     </Datagrid>
   </List>
 );
