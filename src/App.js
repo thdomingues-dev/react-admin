1725bda049ec6b5770b3f41d4989c1539f256329
Thales Domingues <thdomingues.dev@gmail.com>
2021-05-23 21:07:05 -0400

    feat: home page implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 import UserList from './components/UserList';
 import PostList from './components/PostList';
 import PostEdit from './components/PostEdit';
 import PostCreate from './components/PostCreate';
+import Home from './components/Home';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Home} dataProvider={dataProvider}>
     <Resource
       name="posts"
       list={PostList}
       edit={PostEdit}
       create={PostCreate}
+      icon={PostIcon}
     />
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+const Home = () => {
+  return (
+    <Card>
+      <CardHeader title="Hey, Welcome!" />
+      <CardContent>React-admin is here.</CardContent>
+    </Card>
+  );
+};
+
+export default Home;
