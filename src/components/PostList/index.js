4e0ac558257ee13a7a9f086666712dd1b53122ac
Thales Domingues <thdomingues.dev@gmail.com>
2021-05-23 20:41:25 -0400

    feat: PostFilter has been added

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,8 +1,30 @@
 import React from "react";
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Filter,
+  ReferenceInput,
+  SelectInput,
+  TextInput,
+} from 'react-admin';
+
+const PostFilter = (props) => {
+  return (
+    <Filter {...props}>
+      <TextInput label="Search" source="q" alwaysOn />
+
+      <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+    </Filter>
+  );
+};
 
 const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter />} {...props}>
     <Datagrid>
       <TextField source="id" />
 
